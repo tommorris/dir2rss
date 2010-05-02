65af5dc724eccea6e1545fecbeece6f82beb825c
Tom Morris <tom@tommorris.org>
2010-05-02 21:44:43 +0100

    changed port number in dir2rss_serv.rb so the defaults are the same.

--- dir2rss_serv.rb
+++ dir2rss_serv.rb
@@ -7,6 +7,6 @@ require 'mongrel'
 #Daemons.daemonize()
 
 Dir.chdir('/Users/tom/Downloads/')
-h = Mongrel::HttpServer.new("0.0.0.0", "3001")
+h = Mongrel::HttpServer.new("0.0.0.0", "3000")
 h.register('/', Mongrel::DirHandler.new('.'))
 h.run.join
